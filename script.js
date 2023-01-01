c5ea3f2ef648d74e35245c90c4532e1dd8ba3d22
FilipKufalov <filip.kufalov@hotmail.com>
2023-01-01 17:07:12 +0100

    html js css

---
+++ script.js
@@ -0,0 +1,27 @@
+const countDownDate = new Date("Jan 1, 2024 00:00:00").getTime();
+
+// Update the count down every 1 second
+const x = setInterval(function() {
+
+  // Get today's date and time
+  const now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  const distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="countdown"
+  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Happy New Year!";
+  }
+}, 1000);
